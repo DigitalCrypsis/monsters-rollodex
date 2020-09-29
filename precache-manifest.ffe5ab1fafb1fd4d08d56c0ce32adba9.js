a0a400dc0a9fd3d12c2d921a2e3b234cda5cf850
AB <obiter@live.co.uk>
2020-09-29 16:11:07 +0100

    Updates

---
+++ precache-manifest.ffe5ab1fafb1fd4d08d56c0ce32adba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ae15eb6b1613216abfc3df5f7ce2a7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f67b1ea257746a6659e7",
+    "url": "/monsters-rolodex/static/css/main.b848bd25.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f67b1ea257746a6659e7",
+    "url": "/monsters-rolodex/static/js/main.6d1e76be.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
